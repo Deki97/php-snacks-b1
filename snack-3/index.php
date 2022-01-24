426f96ef0600492e1ba4b5ad20473f8548be4003
Deki97 <dejan97@libero.it>
2022-01-24 16:43:00 +0100

    snack-3 done

---
+++ snack-3/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Snack 3</title>
+</head>
+<body>
+    <!-- ## Snack 3
+    Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+    <h3>Array di numeri random generati, non ripetuti</h3>
+    <?php 
+        $numbers = [];
+
+        for($i = 0; $i < 15; $i++) {
+            $random_number = rand(1, 100);
+
+            if(!in_array($random_number, $numbers)) {
+                $numbers = $random_number;
+            }
+
+            echo $numbers . ' ';
+        }
+    ?>
+</body>
+</html>
